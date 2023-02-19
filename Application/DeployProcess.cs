61a18b000d2035d7f2dc770d837cafe5c1e419cd
AD-Sansan\kenya.kinoshita <kenya.kinoshita@sansan.com>
2023-02-19 22:03:03 +0900

    add deploy process

--- Application/DeployProcess.cs
+++ Application/DeployProcess.cs
@@ -12,3 +12,36 @@ internal class BadDeployProcess
     }
 }
 
+internal class DeployProcess: IDisposable
+{
+    private bool _disposed;
+
+    private readonly StreamWriter _report = new("");
+
+    public bool CheckStatus()
+    {
+        _report.WriteLine("Hello World./");
+
+        return true;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+
+            }
+
+            _report?.Close();
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
