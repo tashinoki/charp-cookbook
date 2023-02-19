f80f5bf791cfb3dbc87d058b93019f0b7e631670
AD-Sansan\kenya.kinoshita <kenya.kinoshita@sansan.com>
2023-02-19 21:59:43 +0900

    add bad class

---
+++ Application/DeployProcess.cs
@@ -0,0 +1,14 @@
+﻿namespace charp_cookbook.Application;
+
+internal class BadDeployProcess
+{
+    private readonly StreamWriter _report = new("");
+
+    public bool CheckStatus()
+    {
+        _report.WriteLine("Hello World./");
+
+        return true;
+    }
+}
+
