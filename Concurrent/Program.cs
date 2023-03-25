41e73d4b7dcd1d67a12c24a92558045e81b1155b
kenya kinoshita <kenya.112163@gmail.com>
2023-03-25 23:49:45 +0900

    cancel source

---
+++ Concurrent/Program.cs
@@ -0,0 +1,36 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.ComponentModel.Design.Serialization;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace charp_cookbook.Application;
+
+public class Program
+{
+    private int Count = 0;
+
+    public Program()
+    {
+    }
+
+    public static Task Main()
+    {
+
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(10000)
+        await ConcurrentProcess(cts.Token);
+    }
+
+    private static async Task ConcurrentProcess(CancellationToken token)
+    {
+        while (true)
+        {
+            token.ThrowIfCancellationRequested();
+            Count++;
+            Console.WriteLine(Count);
+            await Task.Delay(1000);
+        }
+    }
+}
